b5717fe6e4875fdb3d958dcad4a73ccc770b8ae7
Elizabeth Lin <elizabethylin@gmail.com>
2014-12-01 13:26:08 -0800

    initial commit

---
+++ CharmBits/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CharmBits
+//
+//  Created by Elizabeth Lin on 12/1/14.
+//  Copyright (c) 2014 Elizabeth Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CharmBits/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  CharmBits
+//
+//  Created by Elizabeth Lin on 12/1/14.
+//  Copyright (c) 2014 Elizabeth Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
